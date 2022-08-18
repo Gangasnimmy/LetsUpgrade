ddb3e31190b791c1e66f63c502d83cc30c98ed63
Gangasnimmy <88794591+Gangasnimmy@users.noreply.github.com>
2022-08-18 17:48:47 +0530

    Add files via upload

---
+++ Program 2_08172022.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	string name = "Gangadevi";
+	int age = 24 ;
+	cout<<"name :" << name << endl;
+	cout<<"age :" << age << endl;
+	return 0;
+}
