b6415e9825961c728c5cf5d34f2268d8f33529f1
Gangasnimmy <88794591+Gangasnimmy@users.noreply.github.com>
2022-09-20 15:18:37 +0530

    Add files via upload

---
+++ Sept 15/ASCII_Value.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char c;
+    cout<<"Enter a character for which you want to see ASCII value: ";
+    cin>>c;
+    cout<<"ASCII value of "<<c<<" is "<<int(c);
+	return 0;
+}
---
+++ Sept 15/Data_Sizecpp.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"size of each data types"<<endl;
+	cout<<"Int: "<<sizeof(int)<<endl;
+	cout<<"Float: "<<sizeof(float)<<endl;
+	cout<<"Double: "<<sizeof(double)<<endl;
+	cout<<"Char: "<<sizeof(char)<<endl;
+	cout<<"Long: "<<sizeof(long)<<endl;
+	cout<<"Short: "<<sizeof(short)<<endl;
+	cout<<"Boolean: "<<sizeof(bool)<<endl;
+	return 0;
+}
