477128ccaf30f6b8cf2e3ff46a8eed5ace037e37
Gangasnimmy <88794591+Gangasnimmy@users.noreply.github.com>
2022-09-20 20:11:32 +0530

    Update VowelOrConsonent.cpp

--- Sept 16/VowelOrConsonent.cpp
+++ Sept 16/VowelOrConsonent.cpp
@@ -5,7 +5,7 @@ int main(){
 	char c;
 	cout<<"Enter the character: ";
 	cin>>c;
-	if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u')
+	if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' || c=='O' || c=='U')
 		cout<<c<<" is vowel"<<endl;
     else cout<<c<<" is consonent"<<endl;
 	return 0;
