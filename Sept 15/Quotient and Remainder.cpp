317a0a697599a75c2e3f6aa31f874fd76bce4fe0
Gangasnimmy <88794591+Gangasnimmy@users.noreply.github.com>
2022-09-20 15:16:56 +0530

    Create Quotient and Remainder.cpp

---
+++ Sept 15/Quotient and Remainder.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b, quotient, remainder;
+    cout<<"Enter a and b value: ";
+    cin>>a>>b;
+    quotient= a/b;
+    remainder= a%b;
+    cout<<"Quotient= "<<quotient<<" and Remainder= "<<remainder<<endl;
+	return 0;
+}
